b6d567cc5713261e618363799c82d2a9eb66aba5
Deceptrax123 <srinitishsri@gmail.com>
2024-04-07 15:32:23 +0530

    Add preprocessing step for inference

--- app.py
+++ app.py
@@ -4,6 +4,9 @@ import keras
 import numpy as np 
 
 model=tf.keras.models.load_model("Model/model.keras")
+means=[50.551818181818184,53.36272727272727,48.14909090909091,25.616243851779544,71.48177921778637,6.469480065256364,103.46365541576817]
+stds=[36.90894257695227,32.97838509495386,50.636418345000635,5.062597617195944,22.25875105745574,0.7737617731081714,54.945896562329025]
+
 app=Flask(__name__)
 
 @app.route('/')
@@ -13,6 +16,7 @@ def man():
 
 @app.route('/predict',methods=['POST'])
 def home():
+    #Get Data
     data1=request.form['a']
     data2=request.form['b']
     data3=request.form['c']
@@ -21,12 +25,22 @@ def home():
     data6=request.form['f']
     data7=request.form['g']
 
-    arr=np.array([[float(data1),float(data2),float(data3),float(data4),float(data5),float(data6),float(data7)]])
+    form_values=[data1,data2,data3,data4,data5,data6,data7]
+    processed_values=list()
+
+    #Pre-Process
+    for c,i in enumerate(form_values):
+        processed_values.append((float(i)-means[c])/stds[c])
+
+    arr=np.array([processed_values])
+
+    #Prediction
     pred=model.predict(arr)
 
-    data=int(np.where(pred[0]==1)[0])
+    data=np.argmax(pred[0])
 
-    return render_template('after.html',data=data)
+    #Render
+    return render_template('predict.html',data=data)
 
 
 if __name__=='__main__':
